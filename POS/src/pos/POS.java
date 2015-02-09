ae0944fc57a3524c8b416092169b46c16db0c2fb
M. A. Alim Mukul <mukul.sustcse@gmail.com>
2015-02-09 22:09:39 +0600

    new commit added

---
+++ POS/src/pos/POS.java
@@ -0,0 +1,40 @@
+package pos;
+
+import javax.swing.UIManager;
+
+public class POS {
+
+    public static void main(String[] args) {
+
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                /*if ("Windows".equals(info.getName())) {
+                 javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                 break;
+                 }*/
+                UIManager.setLookAndFeel("com.jtattoo.plaf.hifi.HiFiLookAndFeel");
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Category.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Category.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Category.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Category.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+
+        //Splash_window sw = new Splash_window();
+        //sw.run();
+        //login l = new login();
+        
+        
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                //new login().setVisible(true);
+                
+                new home_screen().setVisible(true);
+            }
+        });
+    }
+}
